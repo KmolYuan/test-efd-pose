9fb50837ec304487d00cf58c13a6171efe9c6480
KmolYuan <pyslvs@gmail.com>
2024-02-03 23:06:17 +0800

    Update progress

--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,28 @@
-use efd::{na, uvec, PosedEfd2};
-use four_bar::plot2d::*;
+use efd::PosedEfd2;
+use four_bar::plot::*;
 
 fn main() {
-    let vectors = ANGLE
-        .iter()
-        .map(|a| uvec([a.cos(), a.sin()]))
-        .collect::<Vec<_>>();
-    let target_pose = PATH
-        .iter()
-        .zip(&vectors)
-        .map(|(p, v)| na::Point::from(*p) + v.into_inner())
-        .map(|p| [p.x, p.y])
-        .collect::<Vec<_>>();
-    let efd = PosedEfd2::from_vectors_harmonic(PATH, vectors, false, 9);
-    let curve = efd.curve_efd().generate(90);
-    let pose = curve
-        .iter()
-        .zip(efd.pose_efd().generate(90))
-        .map(|(p, v)| na::Point::from(*p) + uvec(v).into_inner())
-        .map(|p| [p.x, p.y])
-        .collect::<Vec<_>>();
+    let vectors = ANGLE.iter().map(|a| [a.cos(), a.sin()]).collect::<Vec<_>>();
+    let mut target_pose = vectors.clone();
+    target_pose.insert(0, [0.; 2]);
+    let mut pos_i = 0;
+    target_pose.iter_mut().reduce(|a, b| {
+        (0..2).for_each(|i| b[i] += POS[pos_i] * a[i]);
+        pos_i += 1;
+        b
+    });
+    target_pose.pop();
+    let efd = PosedEfd2::from_uvec(PATH, vectors, true);
+    // let curve = efd.curve_efd().generate(90);
+    let pose = efd.pose_efd().generate_norm(90);
+    efd.pose_efd()
+        .as_geo()
+        .inverse()
+        .transform_inplace(&mut target_pose);
     let b = SVGBackend::new("test.svg", (1600, 1600));
-    Figure::new(None)
-        .add_line("Target", PATH, Style::Line, RED)
-        .add_line("Target Pose", target_pose, Style::Circle, RED)
-        .add_line("EFD", curve, Style::Line, BLUE)
-        .add_line("EFD Pose", pose, Style::Circle, BLUE)
+    fb::Figure::new(None)
+        .add_line("Target Pose", target_pose, Style::Line, RED)
+        .add_line("EFD Pose", pose, Style::Line, BLUE)
         .legend(LegendPos::Hide)
         .plot(b)
         .unwrap();
@@ -43,3 +40,14 @@ const PATH: &[[f64; 2]] = &[
     [7.8, -4.9],
 ];
 const ANGLE: &[f64] = &[-0.9, 0., 0.7, 1.5, 2.8, -2.3, -2., -1.9, -2.1];
+const POS: &[f64] = &[
+    0.0,
+    0.5215074810676462,
+    0.46154009683390473,
+    0.4628280149313514,
+    0.45993628469767844,
+    0.29396366382157324,
+    0.28782478070497364,
+    0.3232298754173244,
+    0.33076245611534105,
+];
