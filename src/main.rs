d8153d4c9195cffe9dfba88e94da30d002aa79a8
KmolYuan <pyslvs@gmail.com>
2024-02-07 14:50:53 +0800

    Update new method

--- src/main.rs
+++ src/main.rs
@@ -2,26 +2,18 @@ use efd::PosedEfd2;
 use four_bar::plot::*;
 
 fn main() {
+    const LENGTH: f64 = 7.29;
     let vectors = ANGLE.iter().map(|a| [a.cos(), a.sin()]).collect::<Vec<_>>();
-    let mut target_pose = vectors.clone();
-    target_pose.insert(0, [0.; 2]);
-    let mut pos_i = 0;
-    target_pose.iter_mut().reduce(|a, b| {
-        (0..2).for_each(|i| b[i] += POS[pos_i] * a[i]);
-        pos_i += 1;
-        b
-    });
-    target_pose.pop();
+    let target_pose = std::iter::zip(PATH, &vectors)
+        .map(|(p, v)| std::array::from_fn(|i| p[i] + LENGTH * v[i]))
+        .collect::<Vec<_>>();
     let efd = PosedEfd2::from_uvec(PATH, vectors, true);
-    // let curve = efd.curve_efd().generate(90);
-    let pose = efd.pose_efd().generate_norm(90);
-    efd.pose_efd()
-        .as_geo()
-        .inverse()
-        .transform_inplace(&mut target_pose);
+    dbg!(efd.harmonic());
+    let pose = efd.generate(90);
     let b = SVGBackend::new("test.svg", (1600, 1600));
     fb::Figure::new(None)
-        .add_line("Target Pose", target_pose, Style::Line, RED)
+        .add_line("Target", PATH, Style::Line, RED)
+        .add_line("Target Pose", target_pose, Style::Circle, RED)
         .add_line("EFD Pose", pose, Style::Line, BLUE)
         .legend(LegendPos::Hide)
         .plot(b)
@@ -40,14 +32,3 @@ const PATH: &[[f64; 2]] = &[
     [7.8, -4.9],
 ];
 const ANGLE: &[f64] = &[-0.9, 0., 0.7, 1.5, 2.8, -2.3, -2., -1.9, -2.1];
-const POS: &[f64] = &[
-    0.0,
-    0.5215074810676462,
-    0.46154009683390473,
-    0.4628280149313514,
-    0.45993628469767844,
-    0.29396366382157324,
-    0.28782478070497364,
-    0.3232298754173244,
-    0.33076245611534105,
-];
