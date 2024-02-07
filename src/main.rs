f7ed0f9e02fb8a3cf8721c04adcd1e7a9021c3ed
KmolYuan <pyslvs@gmail.com>
2024-02-07 12:56:28 +0800

    Update progress

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,4 @@
 use four_bar::{efd, plot::*};
-use std::f64::consts::PI;
 
 fn main() {
     const CURVE2D: &[[f64; 2]] = &[
@@ -14,25 +13,12 @@ fn main() {
         [1.0, 3.0],
         [1.5, 2.0],
     ];
-    let curve = efd::tests::CURVE2D.to_vec();
+    // let curve = efd::tests::CURVE2D.to_vec();
+    let curve = CURVE2D.to_vec();
     let efd = efd::Efd2::from_curve(&curve, false);
-    dbg!(efd.harmonic());
-    let count = efd
-        .coeffs()
-        .iter()
-        .filter(|m| m.column(0).perp(&m.column(1)) < 0.)
-        .count();
-    dbg!(count);
     let curve_recon = efd.generate(90);
-    let (mut t, _) = efd::get_target_pos(&curve, false);
-    t.iter_mut().for_each(|x| *x += (count % 2) as f64 * PI);
-    let t_norm = efd.generate_by(&t);
-    // let pose = efd.pose_efd().generate_norm(90);
-    // efd.pose_efd()
-    //     .as_geo()
-    //     .inverse()
-    //     .transform_inplace(&mut target_pose);
-    let b = SVGBackend::new("test.svg", (1600, 1600));
+    let (t, _) = efd::get_target_pos(&curve, false);
+    let t_norm = efd.generate_by_t(&t);
     fb::Figure::new(None)
         .add_line("EFD Recon.", vec![curve_recon[0]], Style::Circle, GREEN)
         .add_line("", &curve_recon[..85], Style::Line, GREEN)
@@ -41,6 +27,6 @@ fn main() {
         .add_line("t - θ1", vec![t_norm[0]], Style::Circle, RED)
         .add_line("", &t_norm[..2], Style::DashDottedLine, RED)
         .legend(LegendPos::UL)
-        .plot(b)
+        .plot(SVGBackend::new("test.svg", (1600, 1600)))
         .unwrap();
 }
