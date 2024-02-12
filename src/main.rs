6ad270b5468babda2fb75095291d4ec6e1962615
KmolYuan <pyslvs@gmail.com>
2024-02-12 11:09:15 +0800

    Update progress

--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,34 @@
-use four_bar::{efd, plot::*};
+use four_bar::{csv, efd, plot::*};
 
 fn main() {
-    const CURVE2D: &[[f64; 2]] = &[
-        [1.0, 1.0],
-        [2.0, 1.5],
-        [3.0, 2.0],
-        [3.5, 3.0],
-        [3.0, 4.0],
-        [2.5, 5.0],
-        [2.0, 4.5],
-        [1.5, 4.0],
-        [1.0, 3.0],
-        [1.5, 2.0],
-    ];
+    let w = std::fs::File::open("test2.csv").unwrap();
+    let curve = csv::from_reader(w).unwrap();
+    // const CURVE2D: &[[f64; 2]] = &[
+    //     [1.0, 1.0],
+    //     [2.0, 1.5],
+    //     [3.0, 2.0],
+    //     [3.5, 3.0],
+    //     [3.0, 4.0],
+    //     [2.5, 5.0],
+    //     [2.0, 4.5],
+    //     [1.5, 4.0],
+    //     [1.0, 3.0],
+    //     [1.5, 2.0],
+    // ];
+    // let curve = CURVE2D.to_vec();
     // let curve = efd::tests::CURVE2D.to_vec();
-    let curve = CURVE2D.to_vec();
-    let efd = efd::Efd2::from_curve(&curve, false);
-    let curve_recon = efd.generate(90);
+    let efd = efd::Efd2::from_curve_harmonic(&curve, false, 1);
+    let curve_recon = efd.generate_norm(90);
     let (t, _) = efd::get_target_pos(&curve, false);
-    let t_norm = efd.generate_by_t(&t);
+    let curve = efd.as_geo().inverse().transform(curve);
+    let t_norm = efd.generate_norm_by_t(&t);
     fb::Figure::new(None)
         .add_line("EFD Recon.", vec![curve_recon[0]], Style::Circle, GREEN)
         .add_line("", &curve_recon[..85], Style::Line, GREEN)
         .add_line("Target", vec![curve[0]], Style::Circle, BLUE)
-        .add_line("", &curve[..2], Style::Line, BLUE)
-        .add_line("t - θ1", vec![t_norm[0]], Style::Circle, RED)
-        .add_line("", &t_norm[..2], Style::DashDottedLine, RED)
+        .add_line("", &curve[..], Style::Line, BLUE)
+        .add_line("t-θ+π", vec![t_norm[0]], Style::Circle, RED)
+        .add_line("", &t_norm[..], Style::DashDottedLine, RED)
         .legend(LegendPos::UL)
         .plot(SVGBackend::new("test.svg", (1600, 1600)))
         .unwrap();
